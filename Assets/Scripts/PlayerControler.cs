7e28321f909c7c160a6de7d4744443a896d8873a
Araon39 <115640752+Araon39@users.noreply.github.com>
2025-01-17 13:20:02 +0200

    add blend tree
    добавил плавные переходы

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -1,21 +1,40 @@
 using UnityEngine;
 
-public class PlayerControler : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    private int speed = 20; // Скорость движения игрока
-    public float horizontal; // Горизонтальное направление движения
-    public float vertical;   // Вертикальное направление движения
+    public Animator animator;
 
     // Метод Update вызывается один раз за кадр
     void Update()
     {
-        // Получаем значения осей от клавиатуры или геймпада
-        horizontal = Input.GetAxis("Horizontal"); // Горизонтальное направление (влево/вправо)
-        vertical = Input.GetAxis("Vertical");   // Вертикальное направление (вверх/вниз)
+        // Устанавливаем скорость перемещения объекта
+        float speed = 3f;
 
-        //Движение игрока
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * vertical);
-        //Поворот игрока
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontal);
+        // Устанавливаем скорость вращения объекта
+        float rotationSpeed = 80;
+
+        // Получаем горизонтальное, вертикальное и вращательное движение мыши
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        float mouseRotate = Input.GetAxis("Mouse X");
+
+        // Перемещаем объект вперед с учетом скорости и вертикального ввода
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * Mathf.Abs(vertical));
+
+        // Вращаем объект вокруг оси Y с учетом скорости вращения и ввода мыши
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed * (horizontal + mouseRotate));
+
+        // Проверяем вертикальный ввод и изменяем значение blend
+        if (vertical > 0) // Движение вперёд
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (vertical < 0) // Движение назад
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        // Устанавливаем значение blend для аниматора
+        animator.SetFloat("Blend", Mathf.Abs(vertical));
     }
 }
